61a5145609347edbf7aa75f83e16ac9750618ab2
Guga <g.juma.tm@gmail.com>
2023-02-09 09:40:26 +0800

    created place order screen and api

--- models/Order.js
+++ models/Order.js
@@ -12,14 +12,17 @@ const orderSchema = new mongoose.Schema(
       },
     ],
     shippingAddress: {
-      fullname: { type: String, required: true },
+      fullName: { type: String, required: true },
       address: { type: String, required: true },
-      // city: { type: String, required: true },
+      city: { type: String, required: true },
       postalCode: { type: String, required: true },
-      country: { type: String, required: true },
+      phoneNumber: { type: String, required: true },
     },
     paymentMethod: { type: String, required: true },
-    priceSum: { type: Number, required: true },
+    itemsPrice: { type: Number, required: true },
+    shippingPrice: { type: Number, required: true },
+    taxPrice: { type: Number, required: true },
+    totalPrice: { type: Number, required: true },
     isPaid: { type: Boolean, required: true, default: false },
     paidAt: { type: Date },
     deliveredAt: { type: Date },
@@ -29,6 +32,6 @@ const orderSchema = new mongoose.Schema(
   }
 );
 
-const Order = mongoose.models.Order || mongoose.model('Order', orderSchema); //prettier-ignore
+const Order = mongoose.models.Order || mongoose.model('Order', orderSchema);
 
 export default Order;
